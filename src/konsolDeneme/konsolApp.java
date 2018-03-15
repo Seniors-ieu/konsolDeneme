645c5fa3e990b54970f0a0120a0717939ec4e4cd
Anıl Ulak <anil_ulak@hotmail.com>
2018-03-15 15:17:37 +0300

    Initial commit

---
+++ src/konsolDeneme/konsolApp.java
@@ -0,0 +1,16 @@
+package konsolDeneme;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class konsolApp {
+
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		
+        double a = 1.54156748;
+        System.out.println("a: %.2f \n" + a);
+
+	}
+
+}
